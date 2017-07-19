3e109f4cbf103d6da0367a2bca27d66344d8362c
Graham Lutz <graham@brainbytescreative.com>
2017-07-19 16:35:35 -0400

    adds custom return message

--- server/slackClient.js
+++ server/slackClient.js
@@ -3,6 +3,7 @@
 const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+let rtm = null;
 
 function handleOnAuthenticated(rtmStartData) {
 	console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`)
@@ -10,6 +11,7 @@ function handleOnAuthenticated(rtmStartData) {
 
 function handleOnMessage(message) {
 	console.log(message);
+	rtm.sendMessage('this is a test message', message.channel, () => {})
 }
 
 function addAuthenticatedHandler(rtm, handler) {
@@ -17,7 +19,7 @@ function addAuthenticatedHandler(rtm, handler) {
 }
 
 module.exports.init = function slackClient(token, logLevel) {
-	const rtm = new RtmClient(token, {logLevel: logLevel});
+	rtm = new RtmClient(token, {logLevel: logLevel});
 	addAuthenticatedHandler(rtm, handleOnAuthenticated)
 	rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
 	return rtm;
