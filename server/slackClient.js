d03817f39dbd19578fc0f45e6033080caa753dfa
Graham Lutz <graham@brainbytescreative.com>
2017-07-19 17:11:27 -0400

    adds nlp.ask to message handler

--- server/slackClient.js
+++ server/slackClient.js
@@ -4,13 +4,14 @@ const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 let rtm = null;
+let nlp = null;
 
 function handleOnAuthenticated(rtmStartData) {
 	console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`)
 }
 
 function handleOnMessage(message) {
-	console.log(message);
+	nlp.ask(message.text)
 	rtm.sendMessage('this is a test message', message.channel, () => {})
 }
 
@@ -18,8 +19,9 @@ function addAuthenticatedHandler(rtm, handler) {
 	rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler)
 }
 
-module.exports.init = function slackClient(token, logLevel) {
+module.exports.init = function slackClient(token, logLevel, nlpClient) {
 	rtm = new RtmClient(token, {logLevel: logLevel});
+	nlp = nlpClient;
 	addAuthenticatedHandler(rtm, handleOnAuthenticated)
 	rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
 	return rtm;
