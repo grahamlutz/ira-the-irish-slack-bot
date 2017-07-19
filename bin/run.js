9f7d1c93dcd86cb54da7edc2aaf8c2260af45be3
Graham Lutz <graham@brainbytescreative.com>
2017-07-19 17:10:00 -0400

    adds witClient to server

--- bin/run.js
+++ bin/run.js
@@ -3,13 +3,15 @@
 const slackClient = require('../server/slackClient');
 const server = require('../server/server');
 const http   = require('http');
-
 const app = http.createServer(server);
+
+const witToken = process.env.WIT_API_TOKEN;
+const witClient = require('../server/witClient')(witToken);
 
 const slackToken = process.env.SLACK_API_TOKEN;
 const slackLogLevel = 'verbose';
 
-const rtm = slackClient.init(slackToken, slackLogLevel);
+const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
 rtm.start();
 
 slackClient.addAuthenticatedHandler(rtm, () => app.listen(3001) )
